f40b6a1fc9f8dc9816b3ea727fd9a1d49c0ecb73
은지 <midzy0228@gmail.com>
2023-03-08 16:26:52 +0900

    [1931] 회의실 배정

---
+++ Baekjoon/donggeun/화의실배정.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+meet = []
+
+for i in range(n):
+    a,b= map(int,input().split())
+    meet.append([a,b])
+
+meet = sorted(meet, key=lambda x:(x[1],x[0]))
+ans = 0
+end = 0
+
+for i in range(len(meet)):
+    if end <= meet[i][0]:
+        end = meet[i][1]
+        ans += 1
+print(ans)
