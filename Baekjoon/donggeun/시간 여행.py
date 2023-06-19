f3eeb4c587c62ad8ecab5424e674bfa1d2813798
은지 <midzy0228@gmail.com>
2023-06-19 17:18:54 +0900

    [6051] 시간 여행

--- Baekjoon/donggeun/시간 여행.py
+++ Baekjoon/donggeun/시간 여행.py
@@ -3,14 +3,22 @@ input = sys.stdin.readline
 
 n = int(input())
 problem = []
-time = [[] for _ in range(n)]
-for i in range(n):
+time = dict()
+time[(0,0)] = -1
+time[(0,1)] = 0
+
+for i in range(1, n+1):
     query = list(map(str,input().rstrip().split()))
     if query[0] == 'a':
-        problem.append(int(query[1]))
+        time[(i, 0)] = int(int(query[1]))
+        time[(i,1)] = i-1
+
     elif query[0] == 't':
-        problem = time[int(query[1])-2][::]
-    elif problem:
-        problem.pop()
-    time[i] = problem[::]
-    print(problem[-1] if problem else -1)
+        k = int(query[1])
+        time[(i,0)] = time[(k-1, 0)]
+        time[(i,1)] = time[(k-1, 1)]
+    elif query[0] == "s":
+        time[(i, 0)] = time[(time[(i-1,1)], 0)]
+        time[(i, 1)] = time[(time[(i-1,1)], 1)]
+
+    print(time[(i,0)])
