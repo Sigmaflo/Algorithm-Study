c8d5ea28bb504a61b2c9912a72c5bb60bb8a9487
은지 <midzy0228@gmail.com>
2023-06-26 19:56:19 +0900

    [1339] 단어 수학

---
+++ Baekjoon/donggeun/단어 수학.py
@@ -0,0 +1,27 @@
+# from collections import defaultdict
+input = open("input.txt", "r").readline
+
+n = int(input())
+words = [input().rstrip() for _ in range(n)]
+char = dict()
+
+for word in words:
+    for i in range(len(word)):
+        if word[i] not in char:
+            char[word[i]] = 0
+        char[word[i]] += 10 ** (len(word) - i - 1)
+
+rep = dict()
+num = 9
+
+for key in sorted(char.keys(), key=lambda x:-char[x[0]]):
+    rep[key] = num
+    num -= 1
+
+for i in range(n):
+    tmp = ""
+    for j in range(len(words[i])):
+        tmp += str(rep[words[i][j]])
+    words[i] = int(tmp)
+
+print(sum(words))
